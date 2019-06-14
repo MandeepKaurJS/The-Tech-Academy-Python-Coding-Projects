0e2c78deb2a3731ecaf95757b3c430874bfee6cf
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-06-13 23:17:57 -0700

    Update with views.py

--- TestTechProject/src/mainapp/products/models.py
+++ TestTechProject/src/mainapp/products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+TYPE_CHOICES={
+    ('Appetizers','Appetizers'),
+    ('entrees','entrees'),
+    ('treats','treats'),
+    ('drinks','drinks')
+}
+class Product(models.Model):
+    type=models.CharField(max_length=60,choices=TYPE_CHOICES)
+    name=models.CharField(max_length=60,default="",blank=True,null=False)
+    description=models.TextField(max_length=300,default="",blank=True)
+    price=models.DecimalField(default=0.00,max_digits=10000,decimal_places=2)
+    image=models.CharField(max_length=250,default='',blank=True)
+    objects=models.Manager()
+    def __str__(self):
+        return self.name
