f95d45af322601dbdcef9a21a8123eece16104e1
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-06-14 11:35:21 -0700

    Added a Html files in templates

--- TestTechProject/src/mainapp/mainapp/views.py
+++ TestTechProject/src/mainapp/mainapp/views.py
@@ -2,5 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
+    user=request.user
+    context={
+        'user':user,
 
-    return render(request,"home.html",{})
+    }
+    return render(request,"home.html",context)
