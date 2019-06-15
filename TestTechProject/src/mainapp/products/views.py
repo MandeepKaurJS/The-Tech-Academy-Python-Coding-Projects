c2570f9119afe154fb47d19a75ed8d3c650a90ae
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-06-15 12:27:31 -0700

    Update

--- TestTechProject/src/mainapp/products/views.py
+++ TestTechProject/src/mainapp/products/views.py
@@ -1,21 +1,61 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Product
+from django.http import HttpResponse
 from .forms import ProductForm
-# Create your views here.
+from .models import Product
+
+
+
 def admin_console(request):
-    #a variable we store values
-    products=Product.objects.all()
-    return render(request,'products/products_page.html',{'products':products})
-def details(request,pk):
-    pk=int(pk)
-    item= get_object_or_404(Product,pk=pk)
-    form=ProductForm(data=request.POST or None,instance=item)
-    if request.method =='POST':
+    products = Product.objects.all()
+    return render(request, 'products/products_page.html', {'products': products})
+
+
+def details(request, pk):
+    pk = int(pk)
+    item = get_object_or_404(Product, pk=pk)
+    form = ProductForm(data=request.POST or None, instance=item)
+    if request.method == 'POST':
         if form.is_valid():
-            form2=form.save(commit=False)
+            form2 = form.save(commit=False)
             form2.save()
-            return  redirect('admin_console')
+            return redirect('admin_console')
         else:
             print(form.errors)
     else:
-        return render(request,"products/present_product.html",{'form':form})
+        return render(request, 'products/present_product.html', {'form': form})
+
+
+def delete(request, pk):
+    pk = int(pk)
+    item = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('admin_console')
+    context = {"item": item,}
+    return render(request, "products/confirmDelete.html", context)
+
+
+def confirmed(request):
+    if request.method == 'POST':
+        # creates form instance and binds data to it
+        form = ProductForm(request.POST or None)
+        if form.is_valid():
+            form.delete()
+            return redirect('admin_console')
+    else:
+        return redirect('admin_console')
+
+
+def createRecord(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('admin_console')
+    else:
+        print(form.errors)
+        form = ProductForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'products/createRecord.html', context)
+
