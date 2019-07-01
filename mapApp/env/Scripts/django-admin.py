cb48fe99f339f1be29b087fed889faba0474210b
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-07-01 11:12:22 -0700

    added new project

---
+++ mapApp/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\vrinman dulay\documents\github\the-tech-academy-python-coding-projects\mapapp\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
